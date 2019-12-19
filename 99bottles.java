3855ea3443dce24f029e02980b83861674429ee9
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2019-12-19 11:10:26 +0700

    Updating code for neater output

--- 99bottles.java
+++ 99bottles.java
@@ -5,26 +5,31 @@ public class 99bottles {
       int bottles = 99;
 
       for (int i = 99; i >= 1; i--) {
-        if (bottles > 1) {
+        if (bottles == 99) {
           System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
+          bottles--;
+          System.out.println("Take one down, pass it around, " + bottles + " bottles of beer on the wall...");
+        }
+        if (bottles > 1 && bottles != 99) {
+          System.out.println("\n" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
           bottles--;
           System.out.println("Take one down, pass it around, " + bottles + " bottles of beer on the wall...");
         }
 
         else if (bottles == 2) {
-          System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
+          System.out.println("\n" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
           bottles--;
           System.out.println("Take one down, pass it around, " + bottles + " bottle of beer on the wall...");
         }
 
         else if (bottles == 1) {
-          System.out.println(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer.");
+          System.out.println("\n" + bottles + " bottle of beer on the wall, " + bottles + " bottle of beer.");
           bottles--;
           System.out.println("Take one down, pass it around, " + bottles + " bottles of beer on the wall...");
         }
       }
 
-      System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+      System.out.println("\nNo more bottles of beer on the wall, no more bottles of beer.");
       System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall...");
 
     }
