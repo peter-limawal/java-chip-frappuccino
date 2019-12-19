3f45d240ba40a84bae4aaa98f1c1c69b2c4d72f0
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2019-12-19 12:26:14 +0700

    Creating shell

---
+++ Armstrong Numbers/armstrong.java
@@ -0,0 +1,26 @@
+/*
+Instructions:
+- Learn about armstrong numbers.
+- Define a function that allows the user to check whether a given number is armstrong number or not.
+- Hint: To do this, first determine the number of digits of the given number. Call that n. Then take every digit in the number and raise it to the nth power. Add them, and if your answer is the original number then it is an Armstrong number.
+- Example: Take 1634. Four digits. So, 1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634. So 1634 is an Armstrong number.
+- Tip: All single digit numbers are Armstrong numbers.
+*/
+import java.util.Scanner;
+
+public class armstrong {
+
+  public boolean testArmstrong(int num) {
+
+  }
+
+  public static void main(String[] args) {
+
+    Scanner number = new Scanner(System.in);
+
+    System.out.print("Enter test number (Must be integer): ");
+    long testNum = input.nextLong();
+    decision = testArmstrong(testNum);
+  }
+
+}
