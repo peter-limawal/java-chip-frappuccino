0885ab3be6858bbb9930b85ed22d679acfd76a8b
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2019-12-20 13:31:28 +0700

    Adding comments
    Basic version of code complete

--- Armstrong Numbers/armstrong.java
+++ Armstrong Numbers/armstrong.java
@@ -9,21 +9,21 @@ Instructions:
 import java.util.Scanner;
 
 public class armstrong {
-
+// Declaring variable verdict to determine if number is armstrong or not
   Boolean verdict;
-
+// Method to determine armstrong number
   public Boolean testArmstrong(int num) {
-
+// Declaring variables for armstrong test
     int initial = num;
     int length = (int) (Math.log10(num) + 1);
     int testSum = 0;
-
+// For loop to calculate if armstrong number
     for (int i = length; i > 0; i--) {
       int a = num % 10;
       testSum += Math.pow(a, length);
       num = (num - a) / 10;
     }
-
+// Conditions to determine if armstrong number or not
     if (testSum == initial) {
       verdict = true;
     }
@@ -31,29 +31,30 @@ public class armstrong {
     else if (testSum != initial) {
       verdict = false;
     }
-
+// Value returned for calling the method
     return verdict;
 
   }
 
   public static void main(String[] args) {
-
+// To obtain input
     Scanner number = new Scanner(System.in);
-
+// Setting variable testNum as input
     int testNum = number.nextInt();
+// Print message
     System.out.println("Test number (Must be integer): " + testNum);
-
+// Calling testArmstrong() method
     armstrong test = new armstrong();
     Boolean decision = test.testArmstrong(testNum);
-
+// Message printed if true
     if (decision == true) {
       System.out.println("The integer " + testNum + " is an Armstrong number");
     }
-
+// Message printed if false
     else if (decision == false) {
       System.out.println("The integer " + testNum + " is not an Armstrong number");
     }
-    
+
   }
 
 }
