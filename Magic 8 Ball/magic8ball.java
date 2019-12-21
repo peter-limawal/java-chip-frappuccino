d1f3179ed5d611817ab1f62e3ab4fe9c3b358eb4
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2019-12-21 15:37:38 +0700

    Creating skeleton and instructions for program

---
+++ Magic 8 Ball/magic8ball.java
@@ -0,0 +1,21 @@
+/*
+- Simulate a magic 8-ball.
+- Allow the user to enter their question.
+- Display an in progress message(i.e. "thinking").
+- Create 20 responses, and show a random response.
+- Allow the user to ask another question or quit.
+  ~ Bonus:
+  ~ Add a gui.
+  ~ It must have box for users to enter the question.
+  ~ It must have at least 4 buttons:
+    1. ask
+    2. clear (the text box)
+    3. play again
+    4. quit (this must close the window)
+*/
+
+public class magic8ball {
+  public static void main(String[] args) {
+    
+  }
+}
