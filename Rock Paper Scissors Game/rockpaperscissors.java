5f270064319fd824e29dafb9fadf14d8547a4baa
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2019-12-23 16:06:04 +0700

    Updating code
    This version is incomplete

--- Rock Paper Scissors Game/rockpaperscissors.java
+++ Rock Paper Scissors Game/rockpaperscissors.java
@@ -9,10 +9,48 @@ Instructions:
   ~ Give the player the option to play again.
   ~ Keep a record of the score (e.g. Player: 3 / Computer: 6).
 */
+import java.util.Scanner;
+
 public class rockpaperscissors {
+
+  public Boolean battle(String rps) {
+
+    int player;
+
+    if (rps = "rock") {
+      player = 0;
+    }
+
+    else if (rps = "paper") {
+      player = 1;
+    }
+
+    else if (rps = "scissors") {
+      player = 2;
+    }
+
+    int comp = (int) (Math.random() * 2);
+
+    if (comp == 0) {
+
+    }
+
+  }
 
   public static void main(String[] args) {
 
+    Scanner choice = new Scanner(System.in);
+    String rps = choice.nextLine();
+    rps = rps.toLowerCase();
+
+    if (rps == "rock" || rps == "paper" || rps == "scissors") {
+      System.out.println("You have chosen: " + rps);
+    }
+
+    else {
+      System.out.println("Please select a proper weapon for battle");
+    }
+
   }
-  
+
 }
