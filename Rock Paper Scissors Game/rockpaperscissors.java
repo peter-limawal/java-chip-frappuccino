23cd6f0085c29e7a8bfcb5d89240332d6cfb6a58
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2019-12-23 16:40:09 +0700

    Code test

--- Rock Paper Scissors Game/rockpaperscissors.java
+++ Rock Paper Scissors Game/rockpaperscissors.java
@@ -13,31 +13,62 @@ import java.util.Scanner;
 
 public class rockpaperscissors {
 
-  public Boolean battle(String rps) {
+  String verdict;
+  int comp;
+
+  public void assign(int type) {
+
+    switch (type) {
+      case 0:
+        comp = "rock";
+        break;
+      case 1:
+        comp = "paper";
+        break;
+      case 2:
+        comp = "scissors";
+        break;
+    }
+
+  }
+
+  public void battle(String rps) {
 
     int player;
 
-    if (rps = "rock") {
-      player = 0;
+    switch (rps) {
+      case "rock":
+        player = 0;
+        break;
+      case "paper":
+        player = 1;
+        break;
+      case "scissors":
+        player = 2;
+        break;
     }
 
-    else if (rps = "paper") {
-      player = 1;
+    comp = (int) (Math.random() * 2);
+
+    int x = (player - comp + 3) % 3;
+
+    if (x == 1) {
+      verdict = "win";
     }
 
-    else if (rps = "scissors") {
-      player = 2;
+    else if (x == 2) {
+      verdict = "lose";
     }
 
-    int comp = (int) (Math.random() * 2);
-
-    if (comp == 0) {
-
+    else if (x == 0) {
+      verdict = "draw";
     }
 
   }
 
   public static void main(String[] args) {
+
+    rockpaperscissors object = new rockpaperscissors();
 
     Scanner choice = new Scanner(System.in);
     String rps = choice.nextLine();
@@ -45,10 +76,15 @@ public class rockpaperscissors {
 
     if (rps == "rock" || rps == "paper" || rps == "scissors") {
       System.out.println("You have chosen: " + rps);
-    }
+
+      object.battle(rps);
+
+      System.out.println("Computer has chosen: " + object.assign(comp));
+      System.out.println("You have " + verdict);
 
     else {
       System.out.println("Please select a proper weapon for battle");
+      System.out.println("E.g. Rock, Paper, Scissors");
     }
 
   }
