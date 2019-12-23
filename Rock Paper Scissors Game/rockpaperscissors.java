318ebcbdfd9dbd0ac95b1f4b05390878bb704c4d
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2019-12-23 15:43:42 +0700

    Creating skeleton for rockpaperscissors.java

---
+++ Rock Paper Scissors Game/rockpaperscissors.java
@@ -0,0 +1,18 @@
+/*
+Instructions:
+- Create a rock-paper-scissors game.
+- Ask the player to pick rock, paper or scissors.
+- Have the computer chose its move.
+- Compare the choices and decide who wins.
+- Print the results.
+  ~ Subgoals:
+  ~ Give the player the option to play again.
+  ~ Keep a record of the score (e.g. Player: 3 / Computer: 6).
+*/
+public class rockpaperscissors {
+
+  public static void main(String[] args) {
+
+  }
+  
+}
