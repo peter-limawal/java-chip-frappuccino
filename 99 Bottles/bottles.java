30d766d0d0433a07f97c46a29dfa57cd5634451c
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2019-12-24 16:55:26 +0700

    Compiling .java apps and uploading classes

--- 99 Bottles/bottles.java
+++ 99 Bottles/bottles.java
@@ -5,36 +5,36 @@ Instructions:
 - Besides the phrase "take one down," you may not type in any numbers/names of numbers directly into your song lyrics.
 - Remember, when you reach 1 bottle left, the word "bottles" becomes singular.
 */
-public class 99bottles {
+public class bottles {
 
     public static void main(String[] args) {
 // Declare variable bottles
       int bottles = 99;
-        
+
 // Create for loop, runs 99 times
       for (int i = 99; i >= 1; i--) {
-          
-// Special case if 99 bottles, no \n in front 
+
+// Special case if 99 bottles, no \n in front
         if (bottles == 99) {
           System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
           bottles--;
           System.out.println("Take one down, pass it around, " + bottles + " bottles of beer on the wall...");
         }
-          
+
 // General case
         if (bottles > 2 && bottles != 99) {
           System.out.println("\n" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
           bottles--;
           System.out.println("Take one down, pass it around, " + bottles + " bottles of beer on the wall...");
         }
-          
+
 // Special case if 2 bottles left
         else if (bottles == 2) {
           System.out.println("\n" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
           bottles--;
           System.out.println("Take one down, pass it around, " + bottles + " bottle of beer on the wall...");
         }
-          
+
 // Special case if 1 bottle left
         else if (bottles == 1) {
           System.out.println("\n" + bottles + " bottle of beer on the wall, " + bottles + " bottle of beer.");
@@ -42,7 +42,7 @@ public class 99bottles {
           System.out.println("Take one down, pass it around, " + bottles + " bottles of beer on the wall...");
         }
       }
-        
+
 // Case when 0 bottles left
       System.out.println("\nNo more bottles of beer on the wall, no more bottles of beer.");
       System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall...");
