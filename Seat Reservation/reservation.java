453ba116cbcc0ecfe559e990ef835ba0bf38b3dd
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2020-01-04 09:43:58 +0700

    Creating skeleton for reservation.java

---
+++ Seat Reservation/reservation.java
@@ -0,0 +1,22 @@
+/*
+Instructions:
+- If you've ever been in a concert, you are aware that you buy tickets to be able to reserve a seat inside a stadium.
+  The seat you will be on will have a specific number or code that would enable you to know exactly how far or how close you are to the stage.
+- Create a simple seat reservation program
+- Create a list that would store dashes '-' as a symbol that the seat is still available to take.
+- Define a function that would loop over the list and print out the seats horizontally or in a 3 x 3 position. Refer to this image for reference.
+- Define a second function that would check if the seats are occupied. This should check if the list contains "X" in each element,
+  which is the symbol that we will use if the seat is taken that you will store in a variable.
+  If the variable is equal to 9 (the total number of seats), return True (and break from the loop), and False if not.
+- Create a loop that would have to (1) ask the user for the number of seat he would want to reserve, (2) print out the chairs,
+(3) check if all the seats are occupied and (4) ask the user now if he/she wants to reserve again.
+*/
+import java.util.*;
+
+public class reservation {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
