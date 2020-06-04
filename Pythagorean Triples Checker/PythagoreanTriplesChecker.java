02a30c28532a63b95bd95647192af1600e49646d
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2020-06-04 13:24:51 +0700

    Adding comments

--- Pythagorean Triples Checker/PythagoreanTriplesChecker.java
+++ Pythagorean Triples Checker/PythagoreanTriplesChecker.java
@@ -9,47 +9,55 @@ import java.util.*;
 public class PythagoreanTriplesChecker {
 
   private static boolean checker(int[] sides) {
+// Applying Pythagorean Theorem
     if ( ((sides[0]*sides[0]) + (sides[1]*sides[1])) == (sides[2]*sides[2]) )
       return true;
     return false;
-  }
+  } // checker method
 
   public static void main(String[] args) {
+// Declaring array for sides
     int[] sides = new int[3];
+// Repeat determining variable
     boolean repeat = false;
-
+// To obtain input
     Scanner keyboard = new Scanner(System.in);
-
+// Obtaining verdict input
     System.out.println("Use pythagorean triples checker? (yes/no): ");
     String verdict = keyboard.nextLine();
-
+// Setting repeat variable depending on verdict
     if (verdict.equalsIgnoreCase("yes"))
       repeat = true;
     else if (verdict.equalsIgnoreCase("no"))
       repeat = false;
+// If answer is not yes/no
     else
       throw new IllegalArgumentException("Answer yes or no!");
-
+// While loop
     while (repeat) {
+// Filling in array with side lengths
       for (int i = 1; i <= 3; i++) {
         System.out.print("Side " + i + ": ");
         sides[i - 1] = Integer.parseInt(keyboard.nextLine());
       }
-
+// Sort array in ascending order
       Arrays.sort(sides);
-
+// Output
       System.out.println("Is a Pythagorean Triple?: " + checker(sides));
+// Reobtaining verdict input
       System.out.println("\nUse pythagorean triples checker again? (yes/no): ");
       verdict = keyboard.nextLine();
-
+// Resetting repeat variable depending on verdict
       if (verdict.equalsIgnoreCase("yes"))
         repeat = true;
       else if (verdict.equalsIgnoreCase("no"))
         repeat = false;
+// If answer is not yes/no
       else
         throw new IllegalArgumentException("Answer yes or no!");
     }
-
+// Final output message
     System.out.print("Goodbye!");
-  }
-}
+  } // Main method
+
+} // End class
