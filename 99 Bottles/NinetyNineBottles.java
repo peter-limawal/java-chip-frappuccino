0a063eb031a64d8c5f9ab48b7a2257e21a0f202c
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2020-06-04 12:04:37 +0700

    Adding comments

--- 99 Bottles/NinetyNineBottles.java
+++ 99 Bottles/NinetyNineBottles.java
@@ -39,6 +39,6 @@ public class NinetyNineBottles {
 // Case when 0 bottles left
       System.out.println("\nNo more bottles of beer on the wall, no more bottles of beer.");
       System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall...");
+    } // Main method
 
-    }
-}
+} // End class
